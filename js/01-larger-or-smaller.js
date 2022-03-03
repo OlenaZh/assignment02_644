e65c4ef536c112108e1fe9d7e5360fc5b30f48da
OlenaZh <alyona.mesh@gmail.com>
2022-03-02 17:09:14 -0800

    done

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,6 +1,6 @@
-
-let num1 = prompt('Enter the first number');
-let num2 = prompt('Enter the second number');
+/*eslint-env browser*/
+let num1 = prompt("Enter the first number: ");
+let num2 = prompt("Enter the second number: ");
 
 if (num1 > num2) {
     console.log (`The larger number entered is: ${num1}`);
