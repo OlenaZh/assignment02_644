45dea8d79673fcf4e264cf300754d81e44227c1a
OlenaZh <alyona.mesh@gmail.com>
2022-03-02 16:56:10 -0800

    new changes

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,11 @@
+
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number');
+
+if (num1 > num2) {
+    console.log (`The larger number entered is: ${num1}`);
+} else if (num2 > num1) {
+    console.log (`The larger number entered is: ${num2}`);
+} else {
+    console.log (`The numbers are equal`);
+}
