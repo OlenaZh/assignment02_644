831e1c8bf74949a8b74fc3732cf161b646355e9a
OlenaZh <alyona.mesh@gmail.com>
2022-03-16 21:37:33 -0700

    hitch-Var-2-added

--- js/05-hitchhikers-guide.js
+++ js/05-hitchhikers-guide.js
@@ -31,3 +31,41 @@ if (rate >= 6 && rate <= 10) {
 } else {
     alert ("The game is over. Thank you.");
 } 
+
+// Or Var-2-Zak`s
+/*let choice = prompt('Are you ready to play the game? (y/n)')
+
+if (choice === 'y') {
+    alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.')
+
+    let direction = prompt('Which direction would you like to head (please enter forward, left, or right).')
+
+    switch (direction) {
+        case 'forward':
+            alert('You walk 100 yards and safely make your way out of the cave.')
+            break
+        case 'left':
+            alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!')
+            break
+        case 'right':
+            alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!')
+            break
+        default:
+            alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.')
+            break
+    }
+
+    let rating = prompt('The game is over. Please rate the game between 1 and 10')
+
+    if (rating < 1 || rating > 10) {
+        alert('Thank you for playing the game')
+    } else {
+        if (rating >= 6) {
+            alert('Thank you for playing the game. Please play again.')
+        } else {
+            alert('Thank you for playing the game. We\'re working hard to improve.')
+        }
+    } 
+} else {
+    alert('Thank you for playing the game, please try again some time.')
+}*/
