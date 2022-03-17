1858ce88062a7c30ca3a783ba02f669730da824f
OlenaZh <alyona.mesh@gmail.com>
2022-03-16 21:39:51 -0700

    Larger/smaller-Var2-added

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -9,3 +9,15 @@ if (num1 > num2) {
 } else {
     console.log (`The numbers are equal`);
 }
+
+//Or Var-2-Zak`s
+/*let num1 = parseInt(prompt('Enter the first number'))
+let num2 = parseInt(prompt('Enter the second number'))
+
+if (num1 < num2) {
+    document.write(`${num2} is larger than ${num1}`)
+} else if (num1 > num2) {
+    document.write(`${num1} is larger than ${num2}`)
+} else {
+    document.write(`Both numbers are equal`)
+}*/
